3d10d92262e7d182f9ab7d77f6ced9beb8a7b91c
Jiří Hroník <jiri.hronik@davosro.cz>
2016-02-25 17:14:51 +0100

    Initial Commit

---
+++ SearchFlights/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchFlights
+//
+//  Created by Jiří Hroník on 25.02.16.
+//  Copyright © 2016 Jiří Hroník. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
